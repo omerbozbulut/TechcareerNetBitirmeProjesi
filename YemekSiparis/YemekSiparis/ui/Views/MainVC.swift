200ad3f6aec770a7d5c83970f2e47f8771cad4fb
Ömer Bozbulut <omerbzblt613@gmail.com>
2024-05-23 04:03:37 +0300

    Main VC actions and searchbar

--- YemekSiparis/YemekSiparis/ui/Views/MainVC.swift
+++ YemekSiparis/YemekSiparis/ui/Views/MainVC.swift
@@ -7,11 +7,21 @@
 
 import UIKit
 import SnapKit
+import RxSwift
 
 class MainVC: BaseVC {
     
     lazy var containerView: UIView = {
         return UIView()
+    }()
+    
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.delegate = self
+        searchBar.barTintColor = .white
+        searchBar.tintColor = .black
+        searchBar.placeholder = "Search someting"
+        return searchBar
     }()
     
     private lazy var collectionView: UICollectionView = {
@@ -23,29 +33,32 @@ class MainVC: BaseVC {
         layout.minimumInteritemSpacing = 20
         layout.minimumLineSpacing = 16
         let cellWidth = (width - 70) / 2
-        layout.itemSize = CGSize(width: cellWidth, height: height/2)
+        layout.itemSize = CGSize(width: cellWidth, height: height/1.9)
         layout.scrollDirection = .vertical
         
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.delegate = self
         collectionView.dataSource = self
-        collectionView.backgroundColor = .clear
+        collectionView.backgroundColor = .myGray
         collectionView.register(FoodCollectionCell.self, forCellWithReuseIdentifier: "foodCell")
         collectionView.showsHorizontalScrollIndicator = false
         return collectionView
     }()
     
     var foodList = [Yemekler]()
+    var cartList = [Sepet_yemekler]()
     var viewModel = MainViewModel()
     
     override func setupViews() {
         super.setupViews()
         
-        bindViewModel()
         viewModel.getAllFood()
+        bindViewModel()
+        setNavbar()
         
+        hideKeyboardWhenTappedAround()
         view.addSubview(containerView)
-        containerView.addSubviews([collectionView])
+        containerView.addSubviews([searchBar, collectionView])
     }
     
     override func setupLayout() {
@@ -55,16 +68,42 @@ class MainVC: BaseVC {
             make.edges.equalToSuperview()
         }
         
+        searchBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.leading.trailing.equalToSuperview()
+        }
+        
         collectionView.snp.makeConstraints { make in
-            make.top.equalToSuperview().inset(64)
+            make.top.equalTo(searchBar.snp.bottom)
             make.leading.trailing.equalToSuperview()
             make.bottom.equalToSuperview().inset(16)
         }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        viewModel.getAllFood()
+    }
+    
+    func setNavbar() {
+        self.navigationItem.title = "Foods"
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .myRed
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white, .font: UIFont(name: "Rubik-Medium", size: 22)!]
+        
+        self.navigationController?.navigationBar.standardAppearance = appearance
+        self.navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        self.navigationController?.navigationBar.compactAppearance = appearance
+    }
+    
     func bindViewModel() {
         _ = viewModel.foodList.subscribe { foods in
             self.foodList = foods
+            self.reload()
+        }
+        
+        _ = viewModel.cartList.subscribe { foods in
+            self.cartList = foods
             self.reload()
         }
     }
@@ -78,7 +117,9 @@ extension MainVC: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "foodCell", for: indexPath) as! FoodCollectionCell
         let food = foodList[indexPath.row]
-        cell.configure(with: food)
+        cell.delegate = self
+        let cartFood = cartList.filter({$0.yemekAdi == food.yemekAdi}).first
+        cell.configure(with: food, cartCount: Int(cartFood?.yemekAdet ?? "0") ?? 0)
         return cell
     }
     
@@ -89,6 +130,25 @@ extension MainVC: UICollectionViewDataSource, UICollectionViewDelegate {
     }
 }
 
+extension MainVC: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+    }
+}
+
+extension MainVC: FoodCollectionCellDelegate {
+    func addFoodToCart(yemekID: String) {
+        if let currentFood = foodList.filter({$0.yemekID == yemekID}).first {
+            viewModel.addToCart(yemek: currentFood)
+        }
+    }
+    
+    func removeFoodToCart(yemekAdi: String) {
+        if let currentFood = cartList.filter({$0.yemekAdi == yemekAdi}).first {
+            viewModel.removeFromCart(yemek: currentFood)
+        }
+    }
+}
 
 extension MainVC {
     func reload() {
