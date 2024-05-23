c989d1a927734adb2d5aeefa3de902ea73c7c570
Ömer Bozbulut <omerbzblt613@gmail.com>
2024-05-23 04:03:57 +0300

    Cart View model

--- YemekSiparis/YemekSiparis/ui/ViewModel/CartViewModel.swift
+++ YemekSiparis/YemekSiparis/ui/ViewModel/CartViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import RxSwift
+
+class CartViewModel {
+    var repository = FoodRepository()
+    var cartList = BehaviorSubject<[Sepet_yemekler]>(value: [Sepet_yemekler]())
+    
+    init() {
+         cartList = repository.cartUniqueFoods
+    }
+    
+    func getAllFood() {
+        repository.getCartFoods()
+        repository.getAllFood()
+    }
+    
+    func addToCart(sepet_yemek: Sepet_yemekler) {
+        repository.addToCart(yemek: sepet_yemek)
+        getAllFood()
+    }
+    
+    func removeFromCart(sepet_yemek: Sepet_yemekler) {
+        repository.removeFromCart(yemek: sepet_yemek)
+        getAllFood()
+    }
+}
