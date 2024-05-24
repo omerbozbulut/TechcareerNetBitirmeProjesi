cb5909bc15614e57af52d01fed812553f021b4f7
Ömer Bozbulut <omerbzblt613@gmail.com>
2024-05-24 20:41:38 +0300

    Food Collection Cell

--- YemekSiparis/YemekSiparis/ui/Cell/FoodCollectionCell.swift
+++ YemekSiparis/YemekSiparis/ui/Cell/FoodCollectionCell.swift
@@ -9,17 +9,14 @@ import UIKit
 import SnapKit
 import Kingfisher
 
+protocol FoodCollectionCellDelegate: AnyObject {
+    func addCollectionFoodToCart(yemekID: String, completion: @escaping (Bool) -> ())
+    func removeCollectionFoodToCart(yemekAdi: String, completion: @escaping (Bool) -> ())
+}
+
 class FoodCollectionCell: BaseCollectionViewCell {
     
     lazy var containerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .myGray
-        view.layer.cornerRadius = 10
-        view.clipsToBounds = true
-        return view
-    }()
-    
-    lazy var foodBGView: UIView = {
         let view = UIView()
         view.backgroundColor = .white
         view.layer.cornerRadius = 10
@@ -35,22 +32,23 @@ class FoodCollectionCell: BaseCollectionViewCell {
         return img
     }()
     
-    lazy var addToCartButton: UIButton = {
-        let img = UIImage(systemName: "plus")?.withRenderingMode(.alwaysTemplate) ?? UIImage()
-        img.withTintColor(UIColor.white)
+    lazy var addToCartButton: StepperButtonView = {
+        let view = StepperButtonView()
+        view.delegate = self
+        return view
+    }()
+    
+    lazy var likeButton: UIButton = {
+        let img = UIImage(systemName: "heart")?.withRenderingMode(.alwaysTemplate) ?? UIImage()
 
         let button = UIButton(type: .custom,
                               title: .empty,
                               image: img,
                               alignment: .fill,
-                              contentEdgeInsets: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10),
-                              cornerRadius: 13,
-                              backgroundColor: .myRed,
-                              titleColor: .white)
-        button.contentVerticalAlignment = .fill
-        button.contentHorizontalAlignment = .fill
-        button.layer.masksToBounds = true
-        button.addTarget(self, action: #selector(addToCart), for: .touchUpInside)
+                              backgroundColor: .clear,
+                              titleColor: .red)
+
+        button.addTarget(self, action: #selector(addToFavorite), for: .touchUpInside)
         return button
     }()
     
@@ -59,8 +57,8 @@ class FoodCollectionCell: BaseCollectionViewCell {
                             textAlignment: .left,
                             numberOfLines: 1,
                             textColor: .black,
-                            font: .systemFont(ofSize: 13),
-                            minimumScaleFactor: 0.6,
+                            font: UIFont(name: "Rubik-Medium", size: 15),
+                            minimumScaleFactor: 0.8,
                             backgroundColor: .clear,
                             lineSpacing: nil)
         return label
@@ -71,65 +69,69 @@ class FoodCollectionCell: BaseCollectionViewCell {
                             textAlignment: .left,
                             numberOfLines: 1,
                             textColor: .black,
-                            font: .systemFont(ofSize: 13),
-                            minimumScaleFactor: 0.6,
+                            font:  UIFont(name: "Rubik-Medium", size: 15),
+                            minimumScaleFactor: 0.8,
                             backgroundColor: .clear,
                             lineSpacing: nil)
         
         return label
     }()
     
+    weak var delegate: FoodCollectionCellDelegate?
     var yemek: Yemekler?
+    var cartCount = 0
     
     override func setupViews() {
-        self.addSubview(containerView)
+        contentView.addSubview(containerView)
         
-        containerView.addSubviews([foodBGView, foodNameLabel, foodPriceLabel, addToCartButton])
-        foodBGView.addSubview(foodImage)
+        containerView.addSubviews([foodImage, foodNameLabel, foodPriceLabel, addToCartButton, likeButton])
     }
     
     override func setupLayout() {
         containerView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
-        
-        foodBGView.snp.makeConstraints { make in
+    
+        foodImage.snp.makeConstraints { make in
             make.top.leading.equalToSuperview().offset(8)
             make.trailing.equalToSuperview().offset(-8)
             make.height.equalTo(120)
         }
         
-        foodImage.snp.makeConstraints { make in
-            make.edges.equalToSuperview().inset(4)
-        }
-        
         foodNameLabel.snp.makeConstraints { make in
-            make.top.equalTo(foodImage.snp.bottom).offset(8)
+            make.top.equalTo(foodImage.snp.bottom).offset(16)
             make.leading.trailing.equalToSuperview().inset(8)
-            make.height.equalTo(16)
+            make.height.equalTo(24)
         }
         
         addToCartButton.snp.makeConstraints { make in
             make.bottom.trailing.equalToSuperview().inset(8)
-            make.height.width.equalTo(26)
+            make.width.equalTo(100)
+            make.height.equalTo(32)
         }
         
         foodPriceLabel.snp.makeConstraints { make in
             make.centerY.equalTo(addToCartButton.snp.centerY)
             make.leading.equalToSuperview().inset(8)
             make.trailing.equalTo(addToCartButton.snp.leading).offset(-8)
-            make.height.equalTo(16)
+            make.height.equalTo(24)
+        }
+        
+        likeButton.snp.makeConstraints { make in
+            make.top.trailing.equalToSuperview().inset(10)
+            make.height.width.equalTo(24)
         }
     }
 }
 
 //MARK: Actions
 extension FoodCollectionCell {
-    @objc func addToCart() {
-        print("ADDED CART")
+    @objc func addToFavorite() {
+        print("ADDED FAVORİTE")
     }
     
-    func configure(with yemek: Yemekler) {
+    func configure(with yemek: Yemekler, cartCount: Int) {
+        self.cartCount = cartCount
         self.yemek = yemek
         
         if let url = URL(string: "http://kasimadalan.pe.hu/yemekler/resimler/\(yemek.yemekResimAdi ?? "ayran.png")") {
@@ -138,5 +140,32 @@ extension FoodCollectionCell {
         
         foodNameLabel.text = yemek.yemekAdi
         foodPriceLabel.text = "₺\(yemek.yemekFiyat ?? "10")"
+        addToCartButton.setSelectedCount()
+    }
+}
+
+extension FoodCollectionCell: StepperButtonDelegate {
+    func setSelectedCount() -> String {
+        "\(cartCount)"
+    }
+    
+    func addFoodToCart() {
+        if let yemek {
+            delegate?.addCollectionFoodToCart(yemekID: yemek.yemekID ?? "0", completion: { value in
+                if value {
+                    self.addToCartButton.setSelectedCount()
+                }
+            })
+        }
+    }
+    
+    func removeFoodToCart() {
+        if let yemek {
+            delegate?.removeCollectionFoodToCart(yemekAdi: yemek.yemekAdi ?? "Ayran", completion: { value in
+                if value {
+                    self.addToCartButton.setSelectedCount()
+                }
+            })
+        }
     }
 }
