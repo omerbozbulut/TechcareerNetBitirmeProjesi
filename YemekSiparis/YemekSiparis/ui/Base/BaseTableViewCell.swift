221b78fc2e1c86f543ed2b066972145592697ad6
Ömer Bozbulut <omerbzblt613@gmail.com>
2024-05-24 20:42:00 +0300

    Table cell add Shadow

--- YemekSiparis/YemekSiparis/ui/Base/BaseTableViewCell.swift
+++ YemekSiparis/YemekSiparis/ui/Base/BaseTableViewCell.swift
@@ -19,6 +19,14 @@ class BaseTableViewCell: UITableViewCell {
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
+        self.layer.cornerRadius = 8
+        self.layer.shadowOffset = CGSize(width: 0, height: 3)
+        self.layer.shadowRadius = 3
+        self.layer.shadowOpacity = 0.3
+        self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: .allCorners, cornerRadii: CGSize(width: 8, height: 8)).cgPath
+        self.layer.shouldRasterize = true
+        self.layer.rasterizationScale = UIScreen.main.scale
+        
         setupViews()
         setupLayout()
     }
