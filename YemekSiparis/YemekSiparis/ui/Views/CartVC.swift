362c1ced832f2db0058e7b1654b1edadae37c2fe
Ömer Bozbulut <omerbzblt613@gmail.com>
2024-05-26 04:40:30 +0300

    cart VC components

--- YemekSiparis/YemekSiparis/ui/Views/CartVC.swift
+++ YemekSiparis/YemekSiparis/ui/Views/CartVC.swift
@@ -27,8 +27,94 @@ class CartVC: BaseVC {
         return tableView
     }()
     
+    lazy var checkoutView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .myGray
+        return view
+    }()
+    
+    lazy var totalAmountView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [totalPriceLabel,totalTitleLabel],
+                               axis: .vertical,
+                               alignment: .center,
+                               distribution: .fillProportionally,
+                               spacing: 2)
+        view.backgroundColor = .clear
+        return view
+    }()
+    
+    lazy var minCartView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .myYellow
+        view.layer.cornerRadius = 13
+        return view
+    }()
+    
+    lazy var minCartText: UILabel = {
+        let label = UILabel(text: .plain("Add items worth ₺30 to reach the minimum cart amount."),
+                            textAlignment: .left,
+                            numberOfLines: 1,
+                            textColor: .myText,
+                            font:  UIFont(name: "Rubik-Regular", size: 15),
+                            minimumScaleFactor: 0.8,
+                            backgroundColor: .clear,
+                            lineSpacing: nil)
+        
+        return label
+    }()
+    
+    lazy var progressBar: UIProgressView = {
+        let progressView = UIProgressView(progressViewStyle: .default)
+        progressView.trackTintColor = .lightGray
+        progressView.progressTintColor = .myGreen
+        return progressView
+    }()
+    
+    lazy var totalTitleLabel: UILabel = {
+        let label = UILabel(text: .plain("Total amount"),
+                            textAlignment: .center,
+                            numberOfLines: 1,
+                            textColor: .myText,
+                            font:  UIFont(name: "Rubik-Light", size: 13),
+                            minimumScaleFactor: 0.8,
+                            backgroundColor: .clear,
+                            lineSpacing: nil)
+        
+        return label
+    }()
+    
+    lazy var totalPriceLabel: UILabel = {
+        let label = UILabel(text: .plain("₺15"),
+                            textAlignment: .center,
+                            numberOfLines: 1,
+                            textColor: .black,
+                            font:  UIFont(name: "Rubik-Medium", size: 25),
+                            minimumScaleFactor: 0.8,
+                            backgroundColor: .clear,
+                            lineSpacing: nil)
+        
+        return label
+    }()
+    
+    lazy var checkoutButton: UIButton = {
+        let image = UIImage(named: "shopping-cart")!.withTintColor(.white)
+        let button = UIButton(type: .custom,
+                              title: .plain("Checkout"),
+                              image: image,
+                              alignment: .center,
+                              cornerRadius: 15,
+                              backgroundColor: .myRed,
+                              titleColor: .white)
+        
+        button.addTarget(self, action: #selector(checkout), for: .touchUpInside)
+        return button
+    }()
+    
+    var uniqueCartFoods = [Sepet_yemekler]()
     var cartFoods = [Sepet_yemekler]()
     var viewModel = CartViewModel()
+    var minCartAmount = 70
+    var totalAmount = 0
     
     override func setupViews() {
         super.setupViews()
